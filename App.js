c03e11e178b0eeb4a002bc61305eb6b7a884edd0
SAYEM5522 <md1040582@gmail.com>
2021-10-08 16:55:49 +0600

    Code Update

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { FlatList, Image, StatusBar, StyleSheet, Text, View } from 'react-native';
+import { PanGestureHandler } from 'react-native-gesture-handler';
+import Animated, { Extrapolate, interpolate, useAnimatedGestureHandler, useAnimatedStyle, useSharedValue, withSpring, withTiming } from 'react-native-reanimated';
 
 export default function App() {
 
 
   return (
     <View style={styles.container}>
-     <Text>Hello</Text>
-  
+ 
      <StatusBar/>
     </View>
   );
