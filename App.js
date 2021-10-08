20abe992043522513fa062dd84ef5b22204b3bc8
SAYEM5522 <md1040582@gmail.com>
2021-10-08 10:17:28 +0600

    Code Update

--- App.js
+++ App.js
@@ -1,88 +1,18 @@
 import React from 'react';
 import { FlatList, Image, StatusBar, StyleSheet, Text, View } from 'react-native';
-import Animated, { Extrapolate, interpolate, useAnimatedScrollHandler, useAnimatedStyle, useSharedValue } from 'react-native-reanimated';
-import { Item } from './Component/Data';
-import ItemList from './Component/ItemList';
+
 export default function App() {
 
-  const translationX = useSharedValue(0);
-  const AnimatedFlatlist=Animated.createAnimatedComponent(FlatList)
-  const scrollHandler = useAnimatedScrollHandler((event) => {
-    translationX.value = event.contentOffset.x;
-  });
-  const BackgroundAnimation=useAnimatedStyle(()=>{
-    return{
-      // transform:[{
-      //   translateY:interpolate(translationX.value,[0,100],[0,-150],Extrapolate.CLAMP)
-      // }],
-      // height:interpolate(translationX.value,[0,100],[130,0],Extrapolate.CLAMP),
-      top:interpolate(translationX.value,[0,100],[0,-150],Extrapolate.CLAMP)
-    }
-  })
 
- 
-  const renderItem=({item,index})=>{
-   
-    return(
-      <Animated.View style={[styles.Container]}>
-         <ItemList key={index} color={item.color} translationX={translationX} index={index} />
-      </Animated.View>
-    )
-  }
   return (
     <View style={styles.container}>
-     
-      <Animated.View style={[styles.Header,BackgroundAnimation]}> 
-      <Animated.View style={[styles.RoundView]}/>
-       </Animated.View>
-      {/* <Animated.View style={[styles.ItemList,]}> */}
-        <AnimatedFlatlist
-        data={Item}
-        keyExtractor={(item)=>item.id}
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        renderItem={renderItem}
-        onScroll={scrollHandler}
-        scrollEventThrottle={16}
-        />
-      {/* </Animated.View> */}
+     <Text>Hello</Text>
+  
      <StatusBar/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    // top:20,
-    flex: 1,
-    backgroundColor: '#fff',
-    
-  
-  },
-  Header:{
-    height:130,
-    width:"75%",
-    backgroundColor:'#B6E3DB',
-    alignSelf:'center',
-    marginBottom:15,
-    borderRadius:30,
-    // top:-130
-  },
-  RoundView:{
-    width:60,
-    height:6.5,
-    backgroundColor:'lightgray',
-    alignSelf:'center',
-    borderRadius:5,
-    top:8
-  },
-  ItemList:{
-   top:-100,
-   height:750
-   },
-  Container:{
-    padding:10,
-    // top:130
-    
-  },
+
 });
--- Component/ItemList.js
+++ Component/ItemList.js
@@ -2,40 +2,19 @@ import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import Animated, { Extrapolate, interpolate, useAnimatedStyle } from 'react-native-reanimated'
 
-const ItemList = ({color,index,translationX}) => {
-  const BackgroundAnimation=useAnimatedStyle(()=>{
-    return{
-      backgroundColor:color,
-      position:(index==0)&&"absolute",
-      top:(index==0)?17:0,
-      left:(index==0)?26:20,
-      borderRadius:(index==0)?40:30,
-      top:(index==0)?interpolate(translationX.value,[0,100],[17,-100],Extrapolate.CLAMP):0,
-      height:(index==0)?interpolate(translationX.value,[0,100],[412,750],Extrapolate.CLAMP):430,
-
-      
-    }
-  })
+const ItemList = () => {
+ 
   return (
     
-    <Animated.View style={[styles.Container,BackgroundAnimation]}>
+    <View style={[styles.Container]}>
 
-    </Animated.View>
+    </View>
   )
 }
 
 export default ItemList
 
 const styles = StyleSheet.create({
-  Container:{
-    // height:800,
-    width:310,
-    alignSelf:'center',
-    top:100,
-    borderRadius:30,
-    left:50,
-    zIndex:1000,
-    
-  },
+
 
 })
