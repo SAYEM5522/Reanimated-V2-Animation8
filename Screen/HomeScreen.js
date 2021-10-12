db77512797f05082ba8a1c087d870b37523f740c
SAYEM5522 <md1040582@gmail.com>
2021-10-12 18:45:45 +0600

    HomeScreen Add

---
+++ Screen/HomeScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text>hello</Text>
+    </View>
+  )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({})
