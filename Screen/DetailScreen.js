910873f9e0096a7814512c5c4d1cbf2c09a97dc6
SAYEM5522 <md1040582@gmail.com>
2021-10-13 17:12:20 +0600

    ImageContainer Update

--- Screen/DetailScreen.js
+++ Screen/DetailScreen.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Dimensions, Image, StyleSheet, Text, View } from 'react-native'
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
+import { AntDesign } from '@expo/vector-icons';
 const styles = StyleSheet.create({
   Container:{
     flex:1,
@@ -18,12 +19,27 @@ const styles = StyleSheet.create({
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
-      height: 4,
+      height: 8,
     },
     shadowOpacity: 0.2,
     shadowRadius: 10,
     elevation: 25,
-    borderRadius:10
+    borderRadius:10,
+    position:"relative"
+  },
+  Icon:{
+    position:'absolute',
+    left:20,
+    top:20
+  },
+  TextI:{
+    position:'absolute',
+    bottom:20,
+    fontSize:35,
+    fontWeight:'bold',
+    color:"white",
+    left:30,
+
   }
 })
 const DetailScreen = ({route}) => {
@@ -34,6 +50,8 @@ const DetailScreen = ({route}) => {
      source={{uri:route.params.img}}
      style={styles.Image}
      />
+     <AntDesign name="left" style={styles.Icon} size={24} color="white" />
+     <Text style={styles.TextI}>Favorites</Text>
      </View>
     </View>
   )
