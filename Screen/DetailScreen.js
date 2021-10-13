acf0a66df26b8a18b373dbb662bd8cb98b9f3832
SAYEM5522 <md1040582@gmail.com>
2021-10-13 17:45:44 +0600

    Switch Component Add

--- Screen/DetailScreen.js
+++ Screen/DetailScreen.js
@@ -1,19 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Dimensions, Image, StyleSheet, Text, View } from 'react-native'
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 import { AntDesign } from '@expo/vector-icons';
+import ToggleSwitch from 'toggle-switch-react-native'
 const styles = StyleSheet.create({
   Container:{
     flex:1,
     backgroundColor:'#fff'
   },
   Image:{
-    height:windowHeight/2,
+    height:(windowHeight/2)-20,
     width:windowWidth,
   },
   ImageContainer:{
-    height:windowHeight/2,
+    height:(windowHeight/2)-20,
     width:windowWidth,
     overflow: 'visible',
     shadowColor: "#000",
@@ -40,9 +41,25 @@ const styles = StyleSheet.create({
     color:"white",
     left:30,
 
+  },
+  TopH:{
+    flexDirection:'row',
+    alignItems:'center',
+    justifyContent:'space-between',
+    paddingHorizontal:10,
+    paddingVertical:15,
+    borderBottomColor:'gray',
+   borderBottomWidth:0.3
+  },
+  TopText:{
+    fontSize:16,
+    fontWeight:'700',
+    color:"gray",
   }
 })
 const DetailScreen = ({route}) => {
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
   return (
     <View style={styles.Container}>
       <View style={styles.ImageContainer}>
@@ -52,6 +69,16 @@ const DetailScreen = ({route}) => {
      />
      <AntDesign name="left" style={styles.Icon} size={24} color="white" />
      <Text style={styles.TextI}>Favorites</Text>
+     </View>
+     <View style={styles.TopH}>
+       <Text style={styles.TopText}>Downloads for offline play</Text>
+       <ToggleSwitch
+        isOn={isEnabled}
+        onColor="green"
+        offColor="lightgray"
+        size="large"
+        onToggle={toggleSwitch}
+/>
      </View>
     </View>
   )
